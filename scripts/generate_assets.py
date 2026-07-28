5c941619b4098f79ac680d87d81afffdfaf95bbf
Joe Davies <jwd101@gmail.com>
2026-07-28 11:02:19 +0200

    Generate SVG favicon from logo.png using embedded base64

--- scripts/generate_assets.py
+++ scripts/generate_assets.py
@@ -30,6 +30,18 @@ def generate_assets():
     apple_icon.save("public/apple-touch-icon.png", "PNG")
     print("Generated apple-touch-icon.png")
 
+    # SVG Favicon (embed 128x128 PNG as base64 data URL)
+    fav_128 = img.resize((128, 128), Image.Resampling.LANCZOS)
+    import io
+    import base64
+    buffer = io.BytesIO()
+    fav_128.save(buffer, format="PNG")
+    b64_data = base64.b64encode(buffer.getvalue()).decode("utf-8")
+    svg_content = f'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 128"><image href="data:image/png;base64,{b64_data}" x="0" y="0" width="128" height="128"/></svg>\n'
+    with open("public/favicon.svg", "w") as f:
+        f.write(svg_content)
+    print("Generated favicon.svg (embedded PNG)")
+
     # 2. Generate SEO OG Image (1200x630)
     # We will center the logo on a dark background (#1b1b1b) matching the app's header
     og_width = 1200
